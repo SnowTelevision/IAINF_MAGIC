4b2bffba0f6de2f1e31b4b5b6c69c7e277846ee5
MSS-3120-06\MagicUser\SnowTV <pguan100@gmail.com>
2018-10-10 20:25:41 -0400

    Finished UI stamina bar position adjustment. (Still have some problem in narrow aspect ratio) Added tutorial animation for player walking and picking up object.

---
+++ UnityProject/Assets/Scripts/Events/TutorialEvents/PlayerPickedUpKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detect if the player picks up the key
+/// </summary>
+public class PlayerPickedUpKey : TutorialEvent
+{
+    public ItemInfo key; // The key the player has to pick up
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        // If the player picks up the key
+        if (key.isBeingHeld)
+        {
+            eventFinished = true;
+        }
+
+        base.Update();
+    }
+
+
+}
